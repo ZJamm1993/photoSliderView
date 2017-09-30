1a34a57da0d7033733cb29447ce2962e71766ea9
bangju <bangju@bangjudeMacBook-Pro.local>
2017-09-30 09:36:51 +0800

    Initial Commit

---
+++ PhotoSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoSliderView
+//
+//  Created by bangju on 2017/9/30.
+//  Copyright © 2017年 bangju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoSliderView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoSliderView
+//
+//  Created by bangju on 2017/9/30.
+//  Copyright © 2017年 bangju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
